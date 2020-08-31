32e39cebc8597d61e178de8403b2726552792d2b
Nico <nico.tukiainen@gmail.com>
2020-08-31 19:34:17 +0300

    fixed url

--- nuxt.config.js
+++ nuxt.config.js
@@ -86,7 +86,7 @@ module.exports = {
   },
   proxy: {
     '/products/': {
-      target: `${process.env.BASE_URL}/api/v1/`,
+      target: 'https://nicotukiainen.com/api/v1/',
       changeOrigin: true
     }
   },
