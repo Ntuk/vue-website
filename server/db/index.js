b91e9eee19cbf37a25b7ca5e8e848002b4768c6a
Nico <nico.tukiainen@gmail.com>
2020-08-31 20:10:19 +0300

    removed dotenv

--- server/db/index.js
+++ server/db/index.js
@@ -3,7 +3,6 @@ const keys = require('../keys');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
-require("dotenv").config();
 require("../models/user");
 require("../models/product");
 require("../models/category");
