2442a27a5f849dc0402a93a2a8605683e2a2dfc3
Nico <nico.tukiainen@gmail.com>
2020-09-02 18:24:18 +0300

    zzzzzz

--- server/db/index.js
+++ server/db/index.js
@@ -22,6 +22,7 @@ exports.initSessionStore = function() {
 }
 
 exports.connect = function() {
+  console.log(keys.URI);
   return mongoose.connect(keys.URI, { 
         useNewUrlParser: true, 
         useUnifiedTopology: true, 
