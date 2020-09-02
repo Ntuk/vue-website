31dacfcc528ec4dc630c71ff8feaef58d1c515fe
Nico Tukiainen <nico.tukiainen@gmail.com>
2020-09-02 15:45:26 +0300

    Delete dev.js

--- server/keys/dev.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  MONGODB_URI: "mongodb+srv://nicotukiainen:J3dinpaluu@cluster0-siwa6.mongodb.net/nico-db?retryWrites=true&w=majority",
-  SESSION_SECRET: "fsafasf98asfasf8^421412849h"
-}
